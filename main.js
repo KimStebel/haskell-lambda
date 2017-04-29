9e65b5c33a4a5dab558a5f19877aee831ab887fc
Kim Stebel <kim.stebel@ovoenergy.com>
2017-04-29 16:28:35 +0100

    it works!

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 exports.main = function(event, context, callback) {
   process.env['PATH'] = process.env['PATH'] + ':' + process.env['LAMBDA_TASK_ROOT'];
   var exec = require('child_process').exec;
-  exec('./main haskell', function(error, stdout, stderr) {
+  exec('./hsmain haskell', function(error, stdout, stderr) {
     callback(null, {error: error, stdout: stdout, stderr: stderr}); 
   });
 }

6772265b4670f5632778481aafe91767c1f15893
Kim Stebel <kim.stebel@ovoenergy.com>
2017-05-15 14:21:03 +0100

    add http package dependency

--- haskell-lambda/src/Lib.hs
+++ haskell-lambda/src/Lib.hs
@@ -4,6 +4,12 @@ module Lib
     ) where
 
 import qualified Data.Text.IO as T
+import qualified Network.HTTP as H
+
+someFunc2 :: IO ()
+someFunc2 = T.putStrLn "hello world"
 
 someFunc :: IO ()
-someFunc = T.putStrLn "hello world"
+someFunc = do
+  res <- H.simpleHTTP (H.getRequest "http://httpbin.org/get") >>= fmap (take 1000) . H.getResponseBody
+  putStrLn (show res)
